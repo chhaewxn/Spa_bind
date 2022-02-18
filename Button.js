0406cb8f8e0f269c0699ed9f4e898a070532f46d
sohyundoh <79795051+sohyundoh@users.noreply.github.com>
2022-02-18 11:05:16 +0900

    Add files via upload
    sohuyun lookbook1 button

---
+++ Button.js
@@ -0,0 +1,27 @@
+import './Button.css';
+import React from 'react';
+import styled from 'styled-components';
+
+
+const StyledButton = styled.button`
+
+  margin: 5px 100px 5px 100px;
+  cursor: pointer;
+  line-height: 5;
+  font-size: 30px;
+  padding: 5px 20px 5px 20px;
+  border: 1px solid lightgray;
+  background: #ffffff;
+  width : 80%;
+  diplay: grid;
+  place-content: center;
+  color: Black;
+  &:focus {
+   color: #949595;
+   border: 1px solid black;
+  }
+`
+const Button = ({children}) =>{
+  return <StyledButton className='LookBook'>{children}</StyledButton>;
+}
+export default Button;
---
+++ LookBook.js
@@ -0,0 +1,13 @@
+import Button from "./Button";
+
+function LookBook (){
+    return(
+        <div>
+            <Button>Spring</Button>
+            <Button>Summer</Button>
+            <Button>Fall</Button>
+            <Button>Winter</Button>
+        </div>
+    );
+}
+export default LookBook;
